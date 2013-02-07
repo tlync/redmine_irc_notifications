99cca3a3173ec85af872a60905fa08d436b2605a
Takuya Fujimura <takuya_fujimura@dwango.co.jp>
2013-02-07 15:30:30 +0900

    Increase truncate limit for ticket comments

--- lib/redmine_irc_notifications/helpers.rb
+++ lib/redmine_irc_notifications/helpers.rb
@@ -1,6 +1,6 @@
 module RedmineIrcNotifications
   module Helpers
-    def self.truncate_words(text, length = 20, end_string = '...')
+    def self.truncate_words(text, length = 50, end_string = '...')
       return if text == nil
       words = text.split()
       words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
